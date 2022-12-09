3cfd57b4a71568638e122915bc01d62b02bd83f1
andmansim <91722324+andmansim@users.noreply.github.com>
2022-12-09 15:42:47 +0000

    no quiere ir

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from intertools import setuo
+from setuptools import setup
 
 setup(
-    name = 'paquete'
-    version = '1.0'
-    description = 'Paquete de prueba'
-    author = 'Andrea'
-    package =['paquete','paquete.hola','paquete.adios'] #otra opcion era poner __all__ en el init, ns que es
+    name = 'paquete',
+    version = '1.0',
+    description = 'Paquete de prueba',
+    author = 'Andrea',
+    package =['paquete','paquete.hola','paquete.adios'], #otra opcion era poner __all__ en el init, ns que es
     scripts = ['']
-    
+
 )
+#Se supone que tiene que crear una capeta dist
